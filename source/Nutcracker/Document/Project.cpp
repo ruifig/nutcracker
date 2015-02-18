4522d3a07abfe0dcc3d36d7e423fec5b9b1d6058
ruifig <ruimvfigueira@gmail.com>
2015-02-18 14:15:40 +0000

    - Simplified workspace code - We can now drag folders into the application, to add them to the workspace - Workspace can now be refreshed, to detect any new files existing on the tracked folders

--- source/Nutcracker/Document/Project.cpp
+++ source/Nutcracker/Document/Project.cpp
@@ -10,8 +10,14 @@ namespace document
 static std::unordered_map<u32, std::weak_ptr<ProjectItem>> gAll;
 std::shared_ptr<struct File> createFile(UTF8String fullpath)
 {
+	// If this file is references somewhere (as for example a file editor window has it open, then use that
+	// object)
+	u32 id = FNVHash32::compute(fullpath.c_str(), fullpath.sizeBytes());
+	auto existing = gAll.find(id);
+	if (existing != gAll.end())
+		return std::static_pointer_cast<File>(existing->second.lock());
+
 	auto f = std::shared_ptr<File>(new File(std::move(fullpath)));
-	CZ_ASSERT(gAll.find(f->id.val) == gAll.end());
 	gAll[f->id.val] = f;
 	return f;
 }
@@ -53,9 +59,10 @@ cz::UTF8String ProjectItem::getDirectory() const
 File::File(UTF8String fullpath_) : ProjectItem(ProjectItemType::File, std::move(fullpath_))
 {
 	auto fname = Filename(fullpath);
-	name = fname.getBaseFilename();
+	name = fname.getFilename();
 	extension = ansiToLower(fname.getExtension());
 	filetime = cz::FileTime::get(fullpath, FileTime::kModified);
+	czDebug(ID_Log, "%s (%s) : %u", fullpath.c_str(), name.c_str(), id.val);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -63,7 +70,8 @@ File::File(UTF8String fullpath_) : ProjectItem(ProjectItemType::File, std::move(
 //////////////////////////////////////////////////////////////////////////
 Folder::Folder(UTF8String fullpath_) : ProjectItem(ProjectItemType::Folder, std::move(fullpath_))
 {
-	name = Filesystem::getSingleton().pathStrip(Filename(fullpath).removeBackslash());
+	name = Filesystem::getSingleton().pathStrip(fullpath);
+	czDebug(ID_Log, "%s (%s) : %u", fullpath.c_str(), name.c_str(), id.val);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -87,7 +95,7 @@ void Project::add(const std::shared_ptr<Folder>& parent, const std::shared_ptr<P
 void Project::addFolder(const UTF8String& path)
 {
 	UTF8String fullpath;
-	if (!Filesystem::getSingleton().fullPath(fullpath, path))
+	if (!Filesystem::getSingleton().fullPath(fullpath, Filename(path).removeBackslash()))
 	{
 		throw std::runtime_error(formatString("Error parsing folder '%s'", path.c_str()));
 	}
@@ -96,6 +104,10 @@ void Project::addFolder(const UTF8String& path)
 	{
 		throw std::runtime_error(formatString("'%s' is not a folder", path.c_str()));
 	}
+
+	ProjectItemId id(FNVHash32::compute(fullpath.c_str(), fullpath.sizeBytes()));
+	if (gAll.find(id.val) != gAll.end())
+		return;
 
 	auto f = createFolder(fullpath);
 	auto existing = getFile(f->id);
@@ -141,7 +153,20 @@ std::shared_ptr<File> Project::getFile(const UTF8String& path)
 
 void Project::resyncFolders()
 {
-	CZ_UNEXPECTED();
+	// Keep the names of first level folders, so we know what to refresh
+	std::vector<UTF8String> folders;
+	for (const auto n : m_root->items)
+		folders.push_back(n->fullpath);
+
+	// By doing this, we drop all our references to files/folders,
+	// but other components still keep the references to any files they are using (such as files
+	// being edited at the moment)
+	m_root.reset();
+	m_root = createFolder("");
+
+	for (auto& n : folders)
+		addFolder(n);
+
 }
 
 void Project::populateFromDir(const std::shared_ptr<Folder>& folder)
@@ -154,7 +179,7 @@ void Project::populateFromDir(const std::shared_ptr<Folder>& folder)
 		{
 			case Filesystem::Type::Directory:
 			{
-				cz::UTF8String path = folder->fullpath + f.name + "\\";
+				cz::UTF8String path = folder->fullpath + "\\" + f.name;
 				auto item = createFolder(path);
 				add(folder, item);
 				populateFromDir(item);
@@ -163,7 +188,7 @@ void Project::populateFromDir(const std::shared_ptr<Folder>& folder)
 			case Filesystem::Type::File:
 				if (ansiToLower(Filename(f.name).getExtension()) == "nut")
 				{
-					auto item = createFile(folder->fullpath + f.name);
+					auto item = createFile(folder->fullpath + "\\" + f.name);
 					add(folder, item);
 				}
 			break;
